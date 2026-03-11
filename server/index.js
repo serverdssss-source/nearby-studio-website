07227e02918df79319595b14ef2f2a3411bed1ad
socialcloud123 <freefirestream101@gmail.com>
2026-03-11 16:41:30 +0530

    Configure full VPS hosting and unified Nginx

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ import { sendConfirmationEmail, sendAdminNotification } from './utils/mailer.js'
 
 const { Pool } = pg;
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://nearbystudio.in", "https://www.nearbystudio.in", "http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
